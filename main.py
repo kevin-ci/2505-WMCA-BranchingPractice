5c11cd6dbc67a3aa3e709952a816eff4a4978eed
Julian Elliott <julianpaulelliott@icloud.com>
2025-05-30 14:48:59 +0100

    Add weird al copy

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Hello world!
+
+
+
+
+# As I walk through the valley where I harvest my grain
+# I take a look at my wife and realise she's very plain
+# But that's just perfect for an Amish like me
